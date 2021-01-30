fc4d0c7e31d5a2409aec25b7b2fda84999e65a4e
Hatewait <pandora1983@ya.ru>
2021-01-30 22:21:55 +0300

    сократила код для условий

--- js/main.js
+++ js/main.js
@@ -5,27 +5,19 @@ function getRandomNumber(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
 
-  if (min < 0) {
-    return 'Числа должны быть положительные'
-  }
-
-  if (max <= min) {
-    return 'Число для максимального значения не должно быть меньше или равно минимальному'
+  if (min < 0 || max <= min) {
+    return 'ошибка'
   }
 
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-getRandomNumber(1,9)
+getRandomNumber(1.3, 1.9)
 
 // функция генерации случайного дробного числа
 function getRandomFloatNumber(min, max, int) {
-  if (min < 0) {
-    return 'Числа должны быть положительные'
-  }
-
-  if (max <= min) {
-    return 'Число для максимального значения не должно быть меньше или равно минимальному'
+  if (min < 0 || max <= min) {
+    return 'ошибка'
   }
 
   let result = (Math.random() * (max - min)) + min;
