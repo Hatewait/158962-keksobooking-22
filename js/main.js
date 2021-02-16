837a3453eed6c35d1b937e0698ed4dc9c05aacd1
Hatewait <pandora1983@ya.ru>
2021-02-16 15:25:12 +0300

    fix: внесла правки

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
-import './popup.js';
 import {getSimilarOffers} from './data-create.js';
+import {drawOfferCard} from './popup.js';
 
 const TOTAL_OBJECTS_AMOUNT = 10;
+const offerCards = getSimilarOffers(10);
+const similarOffersContainer = document.querySelector('.map__canvas');
 
 getSimilarOffers(TOTAL_OBJECTS_AMOUNT);
-// console.log(getSimilarOffers(TOTAL_OBJECTS_AMOUNT));
 
-
-
+similarOffersContainer.appendChild(drawOfferCard(offerCards[0]));
--- js/popup.js
+++ js/popup.js
@@ -1,90 +1,62 @@
-import {getSimilarOffers} from './data-create.js';
-import {offerFeatures} from './data-create.js';
-
-const similarOffersContainer = document.querySelector('.map__canvas');
 const similarOfferTemplate = document.querySelector('#card')
   .content
   .querySelector('.popup');
-const fragment = document.createDocumentFragment();
 
-function getTypeOfHousing(item) {
-  let type = item.offer.type;
-
+function getTypeOfHousing(type) {
   switch (type) {
     case 'flat':
-      type = 'Квартира'
-      break;
+      return 'Квартира'
 
     case 'bungalow':
-      type = 'Бунгало'
-      break;
+      return 'Бунгало'
 
     case 'house':
-      type = 'Дом'
-      break;
+      return 'Дом'
 
     case 'palace':
-      type = 'Дворец'
-      break;
+      return 'Дворец'
   }
-
-  return type;
 }
 
 // функция для отрисовки типовой карточки на основе шаблона
-function drawOfferCard(similarOfferItem) {
+export function drawOfferCard(similarOfferItem) {
   const similarElement = similarOfferTemplate.cloneNode(true); // клонируем полностью всю разметку из шаблона
 
   // в блоке, который склонировали ищем элементы по класса и перезаписываем их на свои параметры из переданного сгенерированного объекта
   similarElement.querySelector('.popup__title').textContent = similarOfferItem.offer.title;
   similarElement.querySelector('.popup__text--address').textContent = similarOfferItem.offer.address;
   similarElement.querySelector('.popup__text--price').textContent = `${similarOfferItem.offer.price} ₽/ночь`;
-  similarElement.querySelector('.popup__type').textContent = getTypeOfHousing(similarOfferItem);
-  similarElement.querySelector('.popup__text--capacity').textContent = `${similarOfferItem.offer.rooms} комнаты для ${similarOfferItem.offer.guests} гостей`;
-  similarElement.querySelector('.popup__text--capacity').textContent = `Заезд после ${similarOfferItem.offer.checkin} выезд до ${similarOfferItem.offer.checkout}`;
+  similarElement.querySelector('.popup__type').textContent = getTypeOfHousing(similarOfferItem.offer.type);
+  similarElement.querySelector('.popup__text--time').textContent = `${similarOfferItem.offer.rooms} комнаты для ${similarOfferItem.offer.guests} гостей`;
+  similarElement.querySelector('.popup__text--time').textContent = `Заезд после ${similarOfferItem.offer.checkin} выезд до ${similarOfferItem.offer.checkout}`;
   similarElement.querySelector('.popup__features').textContent = '';
   similarElement.querySelector('.popup__description').textContent = similarOfferItem.offer.description;
   similarElement.querySelector('.popup__photos').textContent = '';
   similarElement.querySelector('.popup__avatar').src = similarOfferItem.author.avatar;
 
-
   // В блок .popup__photos выведите все фотографии из списка offer.photos. Каждая из строк массива photos должна записываться как src соответствующего изображения.
-  for (let i = 0; i < similarOfferItem.offer.photos.length; i++) {
-    const imagesContainer = similarElement.querySelector('.popup__photos');
+  const imagesContainer = similarElement.querySelector('.popup__photos');
+
+  similarOfferItem.offer.photos.forEach((element, index) => {
     const imageItem = document.createElement('img');
     imageItem.classList.add('popup__photo');
     imageItem.alt = 'Фотография жилья';
     imageItem.width = 45;
     imageItem.height = 45;
     imagesContainer.appendChild(imageItem);
-    imageItem.src = similarOfferItem.offer.photos[i];
-  }
+    imageItem.src = similarOfferItem.offer.photos[index];
+  })
 
-  // В список .popup__features выведите все доступные удобства в объявлении.
-  for (let i = 0; i < offerFeatures.length; i++) {
+
+  similarOfferItem.offer.features.forEach((element, index) => {
     const featureList = similarElement.querySelector('.popup__features');
     const featureItem = document.createElement('li');
     featureList.appendChild(featureItem);
-    featureItem.classList.add('popup__feature', 'popup__feature--' + offerFeatures[i]);
-  }
+    featureItem.classList.add('popup__feature', 'popup__feature--' + similarOfferItem.offer.features[index]);
+  })
 
   return similarElement;
 }
 
-const offerCards = getSimilarOffers(10);
-fragment.appendChild(drawOfferCard(offerCards[0]));
-similarOffersContainer.appendChild(fragment);
 
-// функция для отрисовки карточек объявлений
-
-/*function drawAllOfferCards(elements) {
-
-  for (let i = 0; i < elements.length; i++) {
-    fragment.appendChild(drawOfferCard(elements[i]));
-  }
-
-  return similarOffersContainer.appendChild(fragment);
-}
-
-drawAllOfferCards(getSimilarOffers([1]))*/
 
