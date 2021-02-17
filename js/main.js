e8bd205daf679475f6a6e20c938613bbb6f93c29
Hatewait <pandora1983@ya.ru>
2021-02-17 08:55:29 +0300

    fix:3 удалила лишний вызов функции

--- js/main.js
+++ js/main.js
@@ -2,7 +2,6 @@ import {getSimilarOffers} from './data-create.js';
 import {drawOfferCard} from './popup.js';
 
 const TOTAL_OBJECTS_AMOUNT = 10;
-getSimilarOffers(TOTAL_OBJECTS_AMOUNT);
 
-const offerCards = getSimilarOffers(10);
+const offerCards = getSimilarOffers(TOTAL_OBJECTS_AMOUNT);
 drawOfferCard(offerCards[0]);
