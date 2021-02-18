02b5acc84d4d390c37e81d8329b53ebd1d13c22b
Anastasia Migaeva <pandora1983@yandex.ru>
2021-02-18 19:31:05 +0300

    fix: after review

--- js/form.js
+++ js/form.js
@@ -1,13 +1,14 @@
-const AD_FORM_FIELD = document.querySelector('.ad-form__element--time');
-const AD_FORM = document.querySelector('.ad-form');
-const TIME_IN = AD_FORM.querySelector('#timein');
-const TIME_OUT = AD_FORM.querySelector('#timeout');
-const PRICE = AD_FORM.querySelector('#price');
+const adForm = document.querySelector('.ad-form');
+const housingType = adForm.querySelector('#type');
+const adFormTime = document.querySelector('.ad-form__element--time');
+const timeIn = adForm.querySelector('#timein');
+const timeOut = adForm.querySelector('#timeout');
+const price = adForm.querySelector('#price');
+
 const DEFAULT_PRICE = 1000;
 
-PRICE.placeholder = DEFAULT_PRICE;
-PRICE.setAttribute('min', PRICE.placeholder)
-
+price.placeholder = DEFAULT_PRICE;
+price.setAttribute('min', price.placeholder)
 
 const HOUSING_TYPE_PRICES = {
   'bungalow': 0,
@@ -16,36 +17,21 @@ const HOUSING_TYPE_PRICES = {
   'palace': 10000,
 }
 
-export function getPrice() {
-  AD_FORM.addEventListener('change', function (evt) {
-    switch (evt.target.value) {
-      case 'bungalow':
-        PRICE.placeholder = HOUSING_TYPE_PRICES.bungalow;
-        PRICE.setAttribute('min', HOUSING_TYPE_PRICES.bungalow)
-        break;
-
-      case 'flat':
-        PRICE.placeholder = HOUSING_TYPE_PRICES.flat;
-        PRICE.setAttribute('min', HOUSING_TYPE_PRICES.flat)
-        break;
-
-      case 'house':
-        PRICE.placeholder = HOUSING_TYPE_PRICES.house;
-        PRICE.setAttribute('min', HOUSING_TYPE_PRICES.house)
-        break;
-
-      case 'palace':
-        PRICE.placeholder = HOUSING_TYPE_PRICES.palace;
-        PRICE.setAttribute('min', HOUSING_TYPE_PRICES.palace)
-    }
-  });
-}
-
 export function syncTimeHandler() {
-  AD_FORM_FIELD.addEventListener('change', function (evt) {
-    TIME_OUT.value = evt.target.value
-    TIME_IN.value = evt.target.value;
+  adFormTime.addEventListener('change', function (evt) {
+    const optionValue = evt.target.value;
+    timeOut.value = optionValue;
+    timeIn.value = optionValue;
   });
 }
+
+export function getPrice() {
+  housingType.addEventListener('change', function (evt) {
+    const optionValue = evt.target.value;
+    price.placeholder = HOUSING_TYPE_PRICES[optionValue];
+    price.setAttribute('min', HOUSING_TYPE_PRICES[optionValue])
+  });
+}
+
 
 
