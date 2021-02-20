98e3d830134349a6026fbcc985ebc881d9cb45d0
Anastasia Migaeva <pandora1983@yandex.ru>
2021-02-20 16:43:25 +0300

    fixed let to const

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ import {disableForm} from './form.js';
 import {createMap} from './map.js';
 
 const TOTAL_OBJECTS_AMOUNT = 10;
-export let offerCards = getSimilarOffers(TOTAL_OBJECTS_AMOUNT);
+export const offerCards = getSimilarOffers(TOTAL_OBJECTS_AMOUNT);
 getSimilarOffers(TOTAL_OBJECTS_AMOUNT);
 
 syncTimeHandler();
